3df84eaebcf632879842a65182b5dbc2b78844c4
xcf13363175 <13363175@qq.com>
2021-03-12 09:37:20 +0800

    Update lean.sh

--- lean.sh
+++ lean.sh
@@ -13,7 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 sed -i 's/root::0:0:99999:7:::/root:$1$l6Tqa73w$Zoavr1irum3walH.ZOQAo1:18674:0:99999:7:::/g' ./package/base-files/files/etc/shadow
-
+# 去除 luci-app-socat与socat冲突文件
+sed -i '/INSTALL_CONF/d' feeds/packages/net/socat/Makefile
+sed -i '/socat\.init/d' feeds/packages/net/socat/Makefile
 
 # Add luci-app-ssr-plus
 pushd package/lean
@@ -22,7 +24,8 @@ git clone --depth=1 https://github.com/fw876/helloworld
 # Add luci-app-onliner. (need luci-app-nlbwmon)
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
-
+# Add luci-app-socat 
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-socat
 # Add luci-theme-argon
 #git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon
 #git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
